05f276b5545543663d6fdde4e154983e98160c1d
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-02-20 21:05:18 -0600

    added button function

--- guessing-game.js
+++ guessing-game.js
@@ -1,2 +1,28 @@
-console.log("Guessing Game!");
-console.log("it works!");
+console.log('Guessing Game!');
+console.log('it works!');
+
+// Let's guess what this code does!
+
+// Write down what you think each line does.
+
+// It's OK if you're not sure, that's why it's a guess!
+startButton.onclick = function () {
+  alert('Ready, player 1?');
+
+  confirm("Let's play a guessing game.");
+};
+let g = prompt('Guess a number...');
+
+let guess = parseInt(g);
+
+if (guess === 7) {
+  alert('You got it!');
+} else {
+  alert('No!');
+}
+
+alert('Thanks for playing!');
+
+// To run this code, copy it and past it into the Console in the Dev Tools.
+
+// Adapted from Head First Programming by Paul Barry
