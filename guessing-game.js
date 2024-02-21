339b8f7932e814c83e391ca9139b21956ff31dfd
laperkins2 <67718788+laperkins2@users.noreply.github.com>
2024-02-20 21:13:08 -0600

    change formatting

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,5 @@
-console.log('Guessing Game!');
-console.log('it works!');
+// console.log('Guessing Game!');
+// console.log('it works!');
 
 // Let's guess what this code does!
 
@@ -7,22 +7,22 @@ console.log('it works!');
 
 // It's OK if you're not sure, that's why it's a guess!
 startButton.onclick = function () {
-  alert('Ready, player 1?');
+  alert('Are you ready to start the game?');
 
   confirm("Let's play a guessing game.");
+
+  let g = prompt('Guess a number...');
+
+  let guess = parseInt(g);
+
+  if (guess === 7) {
+    alert('You got it!');
+  } else {
+    alert('No!');
+  }
+
+  alert('Thanks for playing!');
 };
-let g = prompt('Guess a number...');
-
-let guess = parseInt(g);
-
-if (guess === 7) {
-  alert('You got it!');
-} else {
-  alert('No!');
-}
-
-alert('Thanks for playing!');
-
 // To run this code, copy it and past it into the Console in the Dev Tools.
 
 // Adapted from Head First Programming by Paul Barry
